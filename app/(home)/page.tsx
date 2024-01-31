145f0a05e19d6aec405dda3b845dc1253e8dc57f
eullergomes <eullertexeira@gmail.com>
2024-01-31 15:27:30 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,31 @@
+import { ptBR } from 'date-fns/locale';
+import { format } from "date-fns"
 import Header from '../_components/header';
-
+import Search from './_components/search';
+import BookingItem from '../_components/booking-item';
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className='text-xl font-bold'>Olá, Euller</h2>
+        <p className="capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem/>
+      </div>
     </div>
   );
 }
