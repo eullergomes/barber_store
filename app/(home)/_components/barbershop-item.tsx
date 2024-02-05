cd967bbf20aae3fd249597c56fb0ae605670d83a
eullergomes <eullertexeira@gmail.com>
2024-02-04 21:11:33 -0300

    feat: add initial service booking

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -23,9 +23,9 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
 
   return ( 
     <Card className='min-w-[167px] max-w-[167px] rounded-2xl'>
-      <CardContent className='px-1 py-0'>
+      <CardContent className='px-1 py-0 pt-1'>
         <div className="w-full h-[159px] relative">
-          <div className='absolute top-2 left-2 z-50'>
+          <div className='absolute top-3 left-3 z-50'>
           <Badge variant='secondary' className='opacity-90 flex gap-1 items-center top-3 left-3'> 
             <StarIcon size={12} className='fill-primary text-primary'/>
             <span className='text-xs'>5,0</span>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -5,6 +5,7 @@ import { db } from '@/app/_lib/prisma';
 import BarbershopInfo from './_components/barbershopInfo';
 import ServiceItem from './_components/serviceItem';
 import { Service } from '@prisma/client';
+import { getServerSession } from 'next-auth';
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -13,6 +14,8 @@ interface BarbershopDetailsPageProps {
 }
 
 const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+
+  const session = await getServerSession() // === useSession in client component
 
   if (!params.id) {
     //TODO: redireciona para home page
@@ -40,7 +43,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
 
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.services.map((service: Service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} barbershop={barbershop}/>
         ))}
       </div>
       
