6f61f16eaa77a802fc4de90b4eb72c1b31099c15
eullergomes <eullertexeira@gmail.com>
2025-01-05 16:02:44 -0300

    fix: update copyright year in footer component

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return ( 
     <footer className='w-full bg-secondary py-6 px-5 space-x-1 lg:px-24 text-center'>
       <span className='text-gray-400 text-xs font-bold opacity-75'>
-        © 2024 Todos os direitos reservados 
+        © 2025 Todos os direitos reservados 
       </span>
       <a href="https://portfolioeuller-euller-gomes-projects.vercel.app/pt" target='_blank' className='text-primary text-xs font-bold'>
         Euller Gomes
